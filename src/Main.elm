16d2110a99c0c4e99d2c66bb161191509f8a0dd5
CeddlyBurge <ceddlyburge@gmail.com>
2019-04-16 19:34:29 +0100

    Add views for all routes and fix "NavBar" title display problem

--- src/Main.elm
+++ src/Main.elm
@@ -59,6 +59,11 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
+    masterViewType model
+    |> renderPage
+
+masterViewType : Model -> BootstrapStarter Msg
+masterViewType model =
     case model.route of
         Home ->
             bootstrapStarterPage 
@@ -66,12 +71,25 @@ view model =
                 (Paragraphs 
                     [ "This is a demonstration website to show the use of Master View Types in Elm"
                     , "It is using a Master View Type that allows the definition and rendering of Html based on the Boostrap Starter Template" ])
-            |> renderPage
-        _ ->
-            div []
-                [ img [ src "/logo.svg" ] []
-                , h1 [] [ text "Your Elm App is working!" ]
-                ]
+        Link ->
+            bootstrapStarterPage 
+                "Example Link" 
+                (Paragraphs 
+                    [ "This is a demonstration website to show the use of Master View Types in Elm"
+                    , "This is the 'Example Link' page" ])
+        DropDown ->
+            bootstrapStarterPage 
+                "Example Drop Down Link" 
+                (Paragraphs 
+                    [ "This is a demonstration website to show the use of Master View Types in Elm"
+                    , "This is the 'Example Drod Down Link' page" ])
+        SearchResults ->
+            bootstrapStarterPage 
+                "Search Results" 
+                (Paragraphs 
+                    [ "This is a demonstration website to show the use of Master View Types in Elm"
+                    , "This is the search results page"
+                    , "You searched for " ++ model.searchExpression ])
 
 
 bootstrapStarterPage: String -> PageContent Msg -> BootstrapStarter Msg 
@@ -82,12 +100,12 @@ bootstrapStarterPage pageTitle pageContent =
             ShowHome
             [
                 Vanilla (NavBarVanilla "Home" ShowHome LinkStateSelected)
-                , Vanilla (NavBarVanilla "Link" ShowLink LinkStateVanilla)
+                , Vanilla (NavBarVanilla "Example Link" ShowLink LinkStateVanilla)
                 , BootstrapStarter.DropDown (NavBarDropDown
                     "Dropdown"
                     "dropdown01"
                     [
-                        NavBarDropDownItem "Action" ShowDropDown 
+                        NavBarDropDownItem "Drop Down Link" ShowDropDown 
                     ])
             ]
             (Search 
